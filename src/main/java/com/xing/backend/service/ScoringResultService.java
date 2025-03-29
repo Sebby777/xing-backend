42bd4935e39b7c5d258b78c38c174fe6bb28b887
Sebby <2798939429@qq.com>
2025-03-29 22:15:38 +0800

    3.29做了generator生成器生成代码的更新

---
+++ src/main/java/com/xing/backend/service/ScoringResultService.java
@@ -0,0 +1,53 @@
+package com.xing.backend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xing.backend.model.dto.scoringResult.ScoringResultQueryRequest;
+import com.xing.backend.model.entity.ScoringResult;
+import com.xing.backend.model.vo.ScoringResultVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 得分结果服务
+ *
+ *
+ * 
+ */
+public interface ScoringResultService extends IService<ScoringResult> {
+
+    /**
+     * 校验数据
+     *
+     * @param scoringResult
+     * @param add 对创建的数据进行校验
+     */
+    void validScoringResult(ScoringResult scoringResult, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param scoringResultQueryRequest
+     * @return
+     */
+    QueryWrapper<ScoringResult> getQueryWrapper(ScoringResultQueryRequest scoringResultQueryRequest);
+    
+    /**
+     * 获取得分结果封装
+     *
+     * @param scoringResult
+     * @param request
+     * @return
+     */
+    ScoringResultVO getScoringResultVO(ScoringResult scoringResult, HttpServletRequest request);
+
+    /**
+     * 分页获取得分结果封装
+     *
+     * @param scoringResultPage
+     * @param request
+     * @return
+     */
+    Page<ScoringResultVO> getScoringResultVOPage(Page<ScoringResult> scoringResultPage, HttpServletRequest request);
+}
